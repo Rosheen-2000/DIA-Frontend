710d3dc43798ac99c2e49a0c880ddf991b36de6c
St2r <St2r@github.com>
2020-08-13 12:31:05 +0800

    模版-开始施工

--- src/app/layout/default/sidebar/sidebar.component.ts
+++ src/app/layout/default/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SpacesService} from './spaces.service';
+import {NzModalService} from 'ng-zorro-antd';
+import {TemplateModalComponent} from '../../../shared/template-modal/template-modal.component';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,11 +11,33 @@ import {SpacesService} from './spaces.service';
 export class SidebarComponent implements OnInit {
 
   constructor(
-    public spacesService: SpacesService
+    public spacesService: SpacesService,
+    private modal: NzModalService
   ) { }
 
   ngOnInit(): void {
     this.spacesService.getSpaces();
   }
 
+  chooseTemplate() {
+    const modal = this.modal.create({
+      nzTitle: '从模版新建文档',
+      nzContent: TemplateModalComponent,
+      // nzViewContainerRef: this.viewContainerRef,
+      nzGetContainer: () => document.body,
+      nzComponentParams: {
+        // title: 'title in component',
+        // subtitle: 'component sub title，will be changed after 2 sec'
+      },
+      nzOnOk: () => new Promise(resolve => setTimeout(resolve, 1000)),
+      nzFooter: [
+        {
+          label: '取消',
+          onClick: () => {
+            modal.destroy();
+          }
+        }
+      ]
+    });
+  }
 }
