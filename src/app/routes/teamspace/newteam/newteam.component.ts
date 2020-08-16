9739f52eecacb52f821e7942287b90e878e76810
Rosheen-2000 <Rosheen2000@163.com>
2020-08-16 19:12:52 +0800

    做好了创建团队页面的样式，但是留了两个bug

--- src/app/routes/teamspace/newteam/newteam.component.ts
+++ src/app/routes/teamspace/newteam/newteam.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-newteam',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewteamComponent implements OnInit {
 
-  constructor() { }
+  validateForm!: FormGroup;
+  listOfControl: Array<{ id: number; controlInstance: string }> = [];
+  selectedValue = null;
+  listOfOption: Array<{ value: string; text: string }> = [];
+  nzFilterOption = () => true;
+
+  search(value: string): void {
+    // this.httpClient
+    //   .jsonp<{ result: Array<[string, string]> }>(`https://suggest.taobao.com/sug?code=utf-8&q=${value}`, 'callback')
+    //   .subscribe(data => {
+    //     const listOfOption: Array<{ value: string; text: string }> = [];
+    //     data.result.forEach(item => {
+    //       listOfOption.push({
+    //         value: item[0],
+    //         text: item[0]
+    //       });
+    //     });
+    //     this.listOfOption = listOfOption;
+    //   });
+  }
+
+  addField(e?: MouseEvent): void {
+    if (e) {
+      e.preventDefault();
+    }
+    const id = this.listOfControl.length > 0 ? this.listOfControl[this.listOfControl.length - 1].id + 1 : 0;
+
+    const control = {
+      id,
+      controlInstance: `passenger${id}`
+    };
+    const index = this.listOfControl.push(control);
+    console.log(this.listOfControl[this.listOfControl.length - 1]);
+    this.validateForm.addControl(this.listOfControl[index - 1].controlInstance, new FormControl(null, Validators.required));
+  }
+
+  removeField(i: { id: number; controlInstance: string }, e: MouseEvent): void {
+    e.preventDefault();
+    if (this.listOfControl.length > 1) {
+      const index = this.listOfControl.indexOf(i);
+      this.listOfControl.splice(index, 1);
+      console.log(this.listOfControl);
+      this.validateForm.removeControl(i.controlInstance);
+    }
+  }
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+    console.log(this.validateForm.value);
+  }
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({});
+    this.addField();
   }
 
 }
