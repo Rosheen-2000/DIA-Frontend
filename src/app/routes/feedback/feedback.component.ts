f0e500d1f872140437e92819828792ea46681a85
Sky <sky_py1044@hotmail.com>
2020-08-19 14:29:33 +0800

    添加了后端需要的后门接口

--- src/app/routes/feedback/feedback.component.ts
+++ src/app/routes/feedback/feedback.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment'
 
 @Component({
   selector: 'app-feedback',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private message: NzMessageService,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  support() {
+    this.message.create('success', '感谢点击，我们已经收到您的支持！')
+    this.http.get<{ msg: string }>(environment.baseUrl + 'doc/checkstatus/');
   }
 
 }
